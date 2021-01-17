de9f3849ce38500f8747ed1acd37d993518b5a47
Lavinya Correia <lavinya.correia01@gmail.com>
2021-01-17 16:32:26 -0300

    Connecting

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -3,16 +3,16 @@ package model.entities;
 import java.io.Serializable;
 
 public class Department implements Serializable {
-
+	
 	private static final long serialVersionUID = 1L;
-
 	private Integer id;
 	private String name;
 	
-	public Department() {
+	public Department(){
 	}
 
 	public Department(Integer id, String name) {
+		super();
 		this.id = id;
 		this.name = name;
 	}
@@ -62,4 +62,7 @@ public class Department implements Serializable {
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
+	
+	
+
 }
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,20 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
-	public List<Department> findAll(){
-		List<Department> list = new ArrayList<>();
-		
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Food"));
-		list.add(new Department(3, "Cellphones"));
-		list.add(new Department(4, "Clothes"));
-		return list;
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
+	public List<Department> findAll(){	
+		return dao.findAll();
 	}
 
 
